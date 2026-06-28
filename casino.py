11b776c7b3e5852d3eab20788cd6314b8ee5d977
Sviatoslav Burlak <burlaksviatoslav08@gmail.com>
2026-06-28 19:33:26 +0100

    I did 67 finder

---
+++ casino.py
@@ -0,0 +1,13 @@
+import random
+
+print("casino")
+while True:
+	x = random.randint(1,100)
+	print(x)
+	if x == 67:
+		print("hahahaha ssiikks seven")
+		break
+	if x == 8:
+		print("yooo its eight")
+		break
+print(f"found {x}")
